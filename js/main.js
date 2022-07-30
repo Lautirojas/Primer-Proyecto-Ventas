fe70f6fdaec76baf2b28678fa9a45cd303155944
Lautaro <lautarorojas572@gmail.com>
2022-07-29 21:28:01 -0300

    corrigiendo links

--- js/main.js
+++ js/main.js
@@ -1,40 +1,7 @@
 
 // REGISTER
 
-let members = [];
 
-class member {
-    constructor(username,email,password,dni,phone) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.dni = dni;
-        this.phone = phone;
-    }
-}
-
-function Login (){
-    let register = new member;
-    members.push(register);
-    console.log(members)
-    return register;
-}
-
-const newUser = () => {
-    let username = prompt("Ingrese su nombre").toLowerCase;
-    let email = prompt("Ingrese su correo electronico").toLowerCase;
-    let password = prompt("Ingrese su contraseña deseada").toLowerCase;
-    let dni = parseInt(prompt("Ingrese su numero de dni"));
-    let phone = prompt("Ingrese su numero de telefono").toLowerCase;
-
-    let register = new member(username,email,password,dni,phone);
-    members.push(register);
-    console.log(members);
-
-    return register;
-}
-
-newUser()
 
 // BUSQUEDA DE PRODUCTO + SU PRECIO
 
@@ -48,33 +15,33 @@ let listaProductos = [
 ]
 
 
-    let search2 = prompt("que producto queres buscar?").toLowerCase();
-    let search3
+    // let search2 = prompt("que producto queres buscar?").toLowerCase();
+    // let search3
 
-    if(search3 = listaProductos.find(el => el.nombre == search2)){
-        alert(`el objeto buscado es ${search3.nombre} y su precio es ${search3.price}`);
-    }else if(search3 === undefined) {
-        alert("El producto que esta buscando no existe o no hay stock");
-    }else{
-        alert("El producto que esta buscando no existe o no hay stock");
-    }
+    // if(search3 = listaProductos.find(el => el.nombre == search2)){
+    //     alert(`el objeto buscado es ${search3.nombre} y su precio es ${search3.price}`);
+    // }else if(search3 === undefined) {
+    //     alert("El producto que esta buscando no existe o no hay stock");
+    // }else{
+    //     alert("El producto que esta buscando no existe o no hay stock");
+    // }
 
 
     // CARRITO
 
     //Suma el todo el precio del los producto agragados al carrito
 
-    const carrito = [];
+    // const carrito = [];
 
-    let totalCarrito = 0;
+    // let totalCarrito = 0;
 
-    function comprar(){    
-        let busquedas = prompt("Que producto deseas comprar?").toLowerCase()   
-        let objeto = listaProductos.find(el => el.nombre == busquedas);   
-        carrito.push(objeto);  
-        totalCarrito = carrito.reduce((cont, el) => cont + el.price, 0);
-        console.log(totalCarrito);
-        console.log(carrito);
-    }
-    comprar()
+    // function comprar(){    
+    //     let busquedas = prompt("Que producto deseas comprar?").toLowerCase();   
+    //     let objeto = listaProductos.find(el => el.nombre == busquedas);   
+    //     carrito.push(objeto);  
+    //     totalCarrito = carrito.reduce((cont, el) => cont + el.price, 0);
+    //     console.log(totalCarrito);
+    //     console.log(carrito);
+    // }
+    // comprar()
     
